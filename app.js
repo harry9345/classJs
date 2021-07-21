a2f63346b0eaa2bb317096ed77d1cc3d18a9d3d4
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-07-21 16:02:45 +0300

    app.js

--- app.js
+++ app.js
@@ -30,22 +30,3 @@ form.addEventListener("submit", (e) => {
     </div>
 `;
 });
-
-let formCounting = document.getElementById("counting");
-let inputCon = document.getElementById("counter");
-let result = document.getElementById("result");
-
-formCounting.addEventListener("keyup", (e) => {
-  e.preventDefault();
-  let str = inputCon.value;
-  let charCount = str.length;
-  var wordCount = str.match(/(\w+)/g).length;
-  let sentenceCount = str.split(/[.!?]+\s/).filter(Boolean).length;
-  result.innerHTML = `
-    <div class="col-sm-12 mt-3 p-5 " style="border: 2px solid red; border-radius: 5px">
-     <p> Number of character  is :${charCount}</p>
-     <p> Number of word  is : ${wordCount}</p>
-     <p> Number of sentence  is : ${sentenceCount}</p>
-    </div>
-`;
-});
